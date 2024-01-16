9e10f3b568a65dc47c5dd8699d6c7da566aed386
Sheriell Dar <asd638796@gmail.com>
2024-01-16 17:53:19 +0000

    figure out triangle rasterisation

--- main.cpp
+++ main.cpp
@@ -42,7 +42,7 @@ void fillBottomFlatTriangle(int v1_x, int v1_y, int v2_x, int v2_y, int v3_x, in
     float curx1 = v1_x;
     float curx2 = v1_x;
 
-    for(int scanlineY = v1_y; scanlineY <= v2_y; scanlineY++){
+    for(int scanlineY = v2_y; scanlineY <= v1_y; scanlineY++){
         line((int) curx1, scanlineY, (int) curx2, scanlineY, image, color);
         curx1 += invslope1;
         curx2 += invslope2;
@@ -58,7 +58,7 @@ void fillTopFlatTriangle(int v1_x, int v1_y, int v2_x, int v2_y, int v3_x, int v
     float curx1 = v3_x;
     float curx2 = v3_x;
 
-    for(int scanlineY = v3_y; scanlineY > v1_y; scanlineY++){
+    for(int scanlineY = v1_y; scanlineY > v3_y; scanlineY--){
         line((int) curx1, scanlineY, (int) curx2, scanlineY, image, color);
         curx1 -= invslope1;
         curx2 -= invslope2;
@@ -67,12 +67,52 @@ void fillTopFlatTriangle(int v1_x, int v1_y, int v2_x, int v2_y, int v3_x, int v
 
 }
 
+
+void drawTriangle(int v1_x, int v1_y, int v2_x, int v2_y, int v3_x, int v3_y, TGAImage &image, TGAColor color)
+{
+   /* at first sort the three vertices by y-coordinate ascending so v1 is the topmost vertice */
+  if(v1_y < v2_y){  
+    swap(v1_y, v2_y);
+    swap(v1_x, v2_x);
+  }
+  
+  if(v2_y < v3_y){
+    swap(v2_y, v3_y);
+    swap(v2_x, v3_x);
+  }
+
+  if(v1_y < v2_y){
+    swap(v1_y, v2_y);
+    swap(v1_x, v2_x);
+  }
+
+  
+  /* check for trivial case of bottom-flat triangle */
+  if (v2_y == v3_y)
+  {
+    fillBottomFlatTriangle(v1_x, v1_y, v2_x, v2_y, v3_x, v3_y, image, color);
+  }
+  /* check for trivial case of top-flat triangle */
+  else if (v1_y == v2_y)
+  {
+    fillTopFlatTriangle(v1_x, v1_y, v2_x, v2_y, v3_x, v3_y, image, color);
+  }
+  else
+  {
+    /* general case - split the triangle in a topflat and bottom-flat one */
+    int v4_x = (int)(v1_x + ((float)(v2_y - v1_y) / (float)(v3_y - v1_y)) * (v3_x - v1_x));
+    int v4_y = v2_y;
+    fillBottomFlatTriangle(v1_x, v1_y, v2_x, v2_y,  v4_x, v4_y, image, color);
+    fillTopFlatTriangle(v2_x, v2_y, v4_x, v4_y, v3_x, v3_y, image, color);
+  }
+}
+
 int main(){
 
     objparser obj_parser("african_head.obj");
     TGAImage image(500, 500, TGAImage::RGB);
 
-    for(vector<int> face : obj_parser.faces){
+    /*for(vector<int> face : obj_parser.faces){
         vector<float> v1 = obj_parser.vertices[face[0] - 1];
         vector<float> v2 = obj_parser.vertices[face[1] - 1];
         vector<float> v3 = obj_parser.vertices[face[2] - 1];
@@ -84,11 +124,13 @@ int main(){
         int v3x = int ((image.get_width() / 2) + image.get_width() / 2 * v3[0]);
         int v3y = int ((image.get_height() / 2) + image.get_height() / 2 * v3[1]);
 
-        line(v1x, v1y, v2x, v2y, image, white);
-        line(v2x, v2y, v3x, v3y, image, white);
-        line(v1x, v1y, v3x, v3y, image, white);
+        drawTriangle(v1x, v1y, v2x, v2y, v3x, v3y, image, TGAColor(rand()%255, rand()%255, rand()%255, 255));
         
-    }
+    }*/
+
+    fillTopFlatTriangle(301, 301, 101, 301, 201, 101, image, TGAColor(rand()%255, rand()%255, rand()%255, 255));
+
+    
     
 	
     image.flip_vertically();
